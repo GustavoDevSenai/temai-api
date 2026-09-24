b91f2e71e6021c8f6a024a8cf674a9c2318aebc0
Gustavo Rodrigues <gustavosoftex@gmail.com>
2026-09-24 15:33:34 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/terabits/temai/TemaiApiApplication.java
@@ -0,0 +1,13 @@
+package com.terabits.temai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemaiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terabits/temai/TemaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terabits.temai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
